fa5fe27c8a19e9fe9554a92d4f51a6a7c43d11e5
Kriegslustig <github@ls7.ch>
2014-12-06 15:56:57 +0100

    Removing a console.log

--- reloadr.js
+++ reloadr.js
@@ -4,7 +4,6 @@ window.addEventListener('load', function(){
   }
   var source = new EventSource(reloadr.reloadSource);
   source.addEventListener('message', function(e){
-    console.log(e.data);
     if(e.data.indexOf('reloadr: ') > -1) {
       var method = e.data.split('reloadr: ')[1];
       reloadr[method]();
