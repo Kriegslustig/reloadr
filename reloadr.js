b056696905528b798a0643e6131055c8a834a6e1
Kriegslustig <github@ls7.ch>
2014-12-06 19:00:49 +0100

    Adding notifications

--- reloadr.js
+++ reloadr.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', function(){
+
+  Notification.requestPermission(function (status) {
+    if (Notification.permission !== status) {
+      Notification.permission = status;
+    }
+  });
+
   reloadr['reload'] = function () {
     location.reload();
   }
@@ -6,6 +13,7 @@ window.addEventListener('load', function(){
   source.addEventListener('message', function(e){
     if(e.data.indexOf('reloadr: ') > -1) {
       var method = e.data.split('reloadr: ')[1];
+        var n = new Notification('Reloadr', {body: location.href.split('//')[1] + ' has reloaded'});
       reloadr[method]();
     }
   }, false)
