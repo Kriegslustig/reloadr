b34f18156873a2c110b583a65ea8fbdd70c8133d
Kriegslustig <github@ls7.ch>
2014-12-08 07:39:43 +0100

    Adding some comments and fixing the syntax

--- event_stream.class.php
+++ event_stream.class.php
@@ -2,11 +2,14 @@
 
 class Event_stream {
 
-  /*
+  /**
    * Adds the needed headers for event-streams
    * Caching can intefeere with event-streams
    *
+   * @access public
+   *
    * @uses header()
+   *
    * @param bool $caching Wether or not caching should be active
    */
   public function set_headers ($caching = false) {
@@ -16,11 +19,15 @@ class Event_stream {
     }
   }
 
-  /*
+  /**
    * Sends an event to the client
    * The event is parsed by reloadr.js
    *
+   * @access public
+   *
    * @param string $data Can be anything it will be revieved by reloadr.js
+   *
+   * @uses json_encode()
    * @uses ob_flush()
    * @uses flush()
    */
--- reloadr.class.php
+++ reloadr.class.php
@@ -6,31 +6,41 @@ class Reloadr {
   private $ignores = ['/.*\.DS_Store/', '/.*\.git.*/'];
   public $event_stream;
 
+  /**
+   * Instanciates the event_stream and sets the needed headers
+   *
+   * @access public
+   *
+   * @uses Event_stream
+   * @uses Reloadr::$event_stream
+   * @uses Reloadr::$event_stream::set_headers
+   */
   public function __construct () {
     $this->event_stream = new Event_stream;
     $this->event_stream->set_headers();
   }
 
-  public function set_callback ($callback) {
-    $this->callback = $callback;
-  }
-
-  /*
+  /**
    * Sets the directory to watch
    *
+   * @access public
+   *
    * @param string $dir The path (relative or absolute) to the dir to watch
-   * @uses reloadr::check_this_dir
+   *
+   * @uses Reloadr::check_this_dir
    */
   public function set_dir ($dir) {
     $this->check_this_dir = $dir;
   }
 
-  /*
+  /**
    * Sets the file watcher
    *
-   * @uses reloadr::create_index
-   * @uses reloadr::set_interval
-   * @uses reloadr::check_for_updates
+   * @access public
+   *
+   * @uses Reloadr::create_index
+   * @uses Reloadr::set_interval
+   * @uses Reloadr::check_for_updates
    */
   public function set_listener () {
     $this->create_index($this->check_this_dir);
@@ -39,22 +49,27 @@ class Reloadr {
     });
   }
 
-  /*
+  /**
    * Set the ignore array
+   *
+   * @access public
    *
    * @param regex array $string Contains all regex to be ignored
    * @var regex array Structure: /regex/, /regex/
-   * @uses reloadr::$ignores
+   *
+   * @uses Reloadr::$ignores
    * @uses explode()
    */
   public function set_ignores($string) {
     $this->ignores = explode(', ', $string);
   }
 
-  /*
+  /**
    * This sends the reload command to the client
    *
-   * @uses reloadr::$event_stream::send_event
+   * @access private
+   *
+   * @uses Reloadr::$event_stream::send_event
    */
   private function reload () {
     $msg = [
@@ -65,13 +80,16 @@ class Reloadr {
     $this->event_stream->send_event($msg);
   }
 
-  /*
-   * Indexes all files in the directory and writes it into reloadr::filemtime_index
+  /**
+   * Indexes all files in the directory and writes it into Reloadr::filemtime_index
+   *
+   * @access private
    *
    * @param path $where Defines the directory to index
    * @type path A relative or absolute path
-   * @uses reloadr::for_every_file
-   * @uses reloadr::$filemtime_index
+   *
+   * @uses Reloadr::for_every_file
+   * @uses Reloadr::$filemtime_index
    * @uses filemtime()
    */
   private function create_index ($where) {
@@ -80,14 +98,17 @@ class Reloadr {
     });
   }
 
-  /*
+  /**
    * Checks all filemtimes against the recorded ones
+   *
+   * @access private
    *
    * @param path $where Defines the directory to check
    * @type path A relative or absolute path
-   * @uses reloadr::for_every_file
-   * @uses reloadr::reload
-   * @uses reloadr::$filemtime_index
+   *
+   * @uses Reloadr::for_every_file
+   * @uses Reloadr::reload
+   * @uses Reloadr::$filemtime_index
    * @uses filemtime
    */
   private function check_for_updates ($where) {
@@ -99,16 +120,18 @@ class Reloadr {
     });
   }
 
-  /*
+  /**
    * Loops through a directory recursively and executes a callback on every file
+   *
+   * @access private
    *
    * @param path $dir The directory to loop through
    * @param anonymous function $callback Is executed on every file
    *
    *     $callback($abs_path);
    *
-   * @uses reloadr::should_ignore
-   * @uses reloadr::for_every_file
+   * @uses Reloadr::should_ignore
+   * @uses Reloadr::for_every_file
    * @uses scandir()
    * @uses realpath()
    * @uses is_dir()
@@ -131,13 +154,16 @@ class Reloadr {
     }
   }
 
-  /*
+  /**
    * Checks if a file or directory should be ignored
+   *
+   * @access private
    *
    * @param path $filepath An absolute filepath
    *
    * @uses this::$ignores
    * @uses preg_match()
+   *
    * @return bool
    */
   private function should_ignore ($filepath) {
@@ -150,11 +176,14 @@ class Reloadr {
     return false;
   }
 
-  /*
+  /**
    * Sets an infinite loop in which a callback is calle
+   *
+   * @access private
    *
    * @param miliseconds $timeout
    * @param anonymous function $callback
+   *
    * @uses $callback()
    * @uses usleep()
    */
